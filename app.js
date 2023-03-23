ada87aa9bb2c39e82bf2765ab2dcd42165e86ac7
Def <deferenseo@gmail.com>
2023-03-23 16:05:19 +0300

    added read/write from/to local storage

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const items = document.querySelector('.items');
 const submitBtn = document.querySelector('.submit');
 const input = document.querySelector('input');
 const removeBtn = document.querySelector('.removeBtn');
+let grocerylist = [];
+
+if (localStorage.getItem('grocerylist')) {
+    grocerylist = JSON.parse(localStorage.getItem('grocerylist'));
+
+    grocerylist.forEach(item => {
+        items.insertAdjacentHTML('afterbegin', `<h2 class="item">- ${item}</h2>`);
+    });
+}
 
 submitBtn.addEventListener('click', () => {
     const item = input.value;
 
     if (item) {
         items.insertAdjacentHTML('afterbegin', `<h2 class="item">- ${item}</h2>`);
+        grocerylist.push(item);
+
+        localStorage.setItem('grocerylist', JSON.stringify(grocerylist));
     }
+
     input.value = '';
     input.focus();
 });
@@ -21,4 +34,6 @@ items.addEventListener('click', (e) => {
 
 removeBtn.addEventListener('click', () => {
     document.querySelectorAll('.item').forEach(item => item.remove());
+    grocerylist = [];
+    localStorage.removeItem('grocerylist');
 });
